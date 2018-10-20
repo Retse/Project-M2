0c5b0ddf2393289953e20d5c02de9688990f606b
Cesar Lugo <cesarlugo00@hotmail.com>
2018-10-21 00:00:35 +0200

    added navbar

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const middlewares = require('../middlewares/middlewares');
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.render('profile');
+router.get('/', middlewares.requireUser, (req, res, next) => {
+  User.find()
+    .then(user => {
+      res.render('profile', { user });
+    });
 });
 
 module.exports = router;
