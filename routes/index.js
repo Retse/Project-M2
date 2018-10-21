1530ba3b9228d8538e8618cdcb1f9a40f6e811c6
Ester Diaz <ester.diaz.sola@gmail.com>
2018-10-21 20:11:58 +0200

    logout finished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Event = require('../models/event');
+const middlewares = require('../middlewares/middlewares');
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', middlewares.userLoggedIn, (req, res, next) => {
   Event.find().limit(3)
     .then(events => {
       res.render('index', { events });
