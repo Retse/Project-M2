5a9ee85425a77f96c35dd1980b3ec67ebbba2548
Ester Diaz <ester.diaz.sola@gmail.com>
2018-10-22 17:50:50 +0200

    eventos perfil & renderización datos usuarios

--- models/user.js
+++ models/user.js
@@ -6,6 +6,8 @@ const userSchema = new Schema({
   password: String,
   email: String,
   dateOfBirth: Date,
+  aboutme: String,
+  quote: String,
   routes: [{
     type: ObjectId,
     ref: 'Event'
